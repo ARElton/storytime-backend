c5ae625b143542397fbed8f39d6a1ac8fed0cb46
ARElton <ameliareneeelton@gmail.com>
2021-02-03 20:00:02 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  resources :storytime_activities
-  resources :child_activities
-  resources :activities
-  resources :child_storytimes
-  resources :categories
-  resources :storytimes
-  resources :children
-  resources :users
+  # resources :storytime_activities
+  # resources :child_activities
+  resources :activities, only: [:index, :show]
+  # resources :child_storytimes
+  # resources :categories
+  resources :storytimes, only: [:index, :show]
+  resources :children, only: [:index, :show]
+  resources :users, only: [:index, :show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
