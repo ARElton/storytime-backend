76f9c6a99bd7f91964ccb0e790f2cb5ebcc64957
ARElton <ameliareneeelton@gmail.com>
2021-02-05 11:01:06 -0500

    fake auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,7 @@
 class UsersController < ApplicationController
 
-    def index
-        users = User.all 
-        render json: users 
-    end
-
-    def show 
-        user = User.find(params[:id])
-        render json: user
+    def autologin
+        render json: @current_user
     end
 
 end
