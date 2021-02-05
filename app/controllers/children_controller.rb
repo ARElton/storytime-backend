4ab2cd03a4b5f170f17ecad52e3673a0239e6437
ARElton <ameliareneeelton@gmail.com>
2021-02-05 15:58:02 -0500

    show method

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -4,5 +4,10 @@ class ChildrenController < ApplicationController
         children = Child.all 
         render json: children 
     end
+
+    def show
+        child = Child.find(params[:id])
+        render json: child 
+    end
     
 end
