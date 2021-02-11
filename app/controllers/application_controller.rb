f34a1d785f7a0c5f33788c256532be93f37647b1
ARElton <ameliareneeelton@gmail.com>
2021-02-11 17:43:52 -0500

    more auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::API
     before_action :authenticate 
 
-    def authenticate
-        @current_user = User.first
+    # def authenticate
+    #     @current_user = User.first
+    # end
+
+    def authorize 
+        @user = AuthorizeRequest.new(request.headers).user
+
+        unless @user
+            render json: { error: "Unauthorized request" }, status: :unauthorized 
+        end
     end
 
 end
